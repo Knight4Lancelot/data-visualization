b60796e12e87c586fa6978b137c1edcc1b849811
Knight4Lancelot <320062713@qq.com>
2022-08-24 15:20:00 +0800

    1

--- src/main/java/com/example/demo01/Controller.java
+++ src/main/java/com/example/demo01/Controller.java
@@ -2,11 +2,17 @@ package com.example.demo01;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Controller {
     @RequestMapping("/index")
     public String hello() {
-        return "Hello Spring Boot!";
+        return "hello world!";
+    }
+    @RequestMapping("/task")
+    public ModelAndView itemsList() {
+        ModelAndView mav = new ModelAndView("/views/index.html");
+        return mav;
     }
 }
